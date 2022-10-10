70aad3396cc8ad20a861fd6e725ada64477421f3
ChocolateCupcake25 <92889263+ChocolateCupcake25@users.noreply.github.com>
2022-10-10 20:55:24 +0100

    Add files via upload

---
+++ Language Translator.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 18:39:00 2022
+
+@author: Fierce Bird // Ziyah Virani
+"""
+from tkinter import*
+from tkinter import ttk
+from googletrans import Translator
+
+root = Tk()
+root.geometry("800x700")
+root.title("language translator")
+root.configure(bg="CadetBlue1")
+
+
+
+label_heading=Label(root,text="LANGUAGE TRANSLATOR",font=("Bookman Old Style",20,"bold","italic"),bg="CadetBlue1",fg="#030140")
+
+label_EnterText=Label(root,text="Enter Text : ",font=("times",15,"bold"),bg="CadetBlue1",fg="#030140")
+
+label_Output=Label(root,text="Output : ",font=("times",15,"bold"),bg="CadetBlue1",fg="#030140")
+
+label_heading.place(relx=0.5,rely=0.1,anchor=CENTER)
+label_EnterText.place(relx=0.08,rely=0.2,anchor=CENTER)
+label_Output.place(relx=0.6,rely=0.2,anchor=CENTER)
+
+Entertext_area=Text(root,bg="LightBlue1",fg="#030140",font=("times",15,"bold"),height=10,wrap=WORD,width=50,padx=10,pady=10,bd=2)
+Entertext_area.place(relx=0.2,rely=0.45,anchor=CENTER)
+
+Outputtext_area=Text(root,bg="LightBlue1",fg="#030140",font=("times",15,"bold"),height=10,wrap=WORD,width=50,padx=15,pady=10,bd=2)
+Outputtext_area.place(relx=0.75,rely=0.45,anchor=CENTER)
+
+root.mainloop()
+
+
+
+
+
+
+
